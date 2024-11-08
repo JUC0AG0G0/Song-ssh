5b90280b5d61e132362e5f8684fbc22455ab835f
jules.corneille <jules.corneille@uha.fr>
2024-11-08 14:18:08 +0100

    selection de la machine qui jouera le son

--- server.js
+++ server.js
@@ -2,19 +2,29 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const { Client } = require('ssh2');
-
+const fs = require('fs');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(express.static('public'));
 
+function loadMachines() {
+  const data = fs.readFileSync('machines.json');
+  return JSON.parse(data);
+}
+
+app.get('/machines', (req, res) => {
+  const machines = loadMachines();
+  res.json(machines);
+});
+
 io.on('connection', (socket) => {
   console.log('Client connecté');
 
-  const sound_url = 'https://www.myinstants.com/media/sounds/deg-deg_4M6Cojn.mp3';
+  const sound_url = 'https://www.myinstants.com/media/sounds/dry-fart.mp3';
 
-  const script = `
+  const script = (sound_url) => `
     # Vérifie si les outils nécessaires sont installés
     if ! command -v wget &> /dev/null || ! command -v alsamixer &> /dev/null || ! command -v mpv &> /dev/null; then
         echo "wget, alsamixer, ou mpv n'est pas installé. Installation en cours..."
@@ -34,23 +44,32 @@ io.on('connection', (socket) => {
     ls ~/Téléchargements
 
     # Joue le son en utilisant mpv
-    for i in {1..1}; do
-        amixer set Master 80%
-        mpv ~/Téléchargements/hihihafunnysound.mp3
-    done
+    mpv ~/Téléchargements/hihihafunnysound.mp3
 
     # Supprime le fichier audio
     rm ~/Téléchargements/hihihafunnysound.mp3
   `;
 
-  socket.on('ssh-connect', () => {
+  socket.on('ssh-connect', (machineId) => {
+    const machines = loadMachines();
+    const machine = machines.find(m => m.id === machineId);
+
+    if (!machine) {
+      socket.emit('ssh-data', 'Machine non trouvée');
+      return;
+    }
+
+    console.log(`Connexion SSH demandée pour la machine : ${machine.nom}`);
+    
     const conn = new Client();
     
     conn.on('ready', () => {
       console.log('Connexion SSH prête');
       socket.emit('ssh-data', 'Connexion SSH établie ! Exécution du script...');
 
-      conn.exec(script, (err, stream) => {
+      const execScript = script(sound_url);
+
+      conn.exec(execScript, (err, stream) => {
         if (err) {
           socket.emit('ssh-data', 'Erreur: ' + err.message);
           return conn.end();
@@ -63,10 +82,10 @@ io.on('connection', (socket) => {
         });
       });
     }).connect({
-        host: '',
+        host: machine.adresse_ip,
         port: 22,
-        username: '',
-        password: ''
+        username: machine.utilisateur,
+        password: machine.mot_de_passe
     });
 
     conn.on('error', (err) => {
